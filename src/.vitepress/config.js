2113aae2cbc06c6295c8b8a977023774f0560b23
Ivan <ipkozyrin@gmail.com>
2024-06-17 22:40:00 -0300

    WIP

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -1,38 +1,43 @@
-import path from 'path'
-import fs from 'fs'
-import { DEFAULT_ENCODE } from 'vitepress-sls-blog-tmpl/src/constants.js'
-import { removeH1Plugin } from 'vitepress-sls-blog-tmpl/src/helpers/mdit-remove-h1.js'
-import { transformTitle } from 'vitepress-sls-blog-tmpl/src/helpers/transformTitle.js'
-import { includeAllPostPreview } from 'vitepress-sls-blog-tmpl/src/helpers/includeAllPostPreview.js'
-import { makeCommonTheme } from './themeLocaleconfig.js'
-import en from './locales/en'
-import ru from './locales/ru'
+import { defineConfig } from "vitepress";
+// import path from "path";
+// import fs from "fs";
+// import { DEFAULT_ENCODE } from "vitepress-sls-blog-tmpl/src/constants.js";
+import { removeH1Plugin } from "vitepress-sls-blog-tmpl/src/helpers/mdit-remove-h1.js";
+import { transformTitle } from "vitepress-sls-blog-tmpl/src/helpers/transformTitle.js";
+import { includeAllPostPreview } from "vitepress-sls-blog-tmpl/src/helpers/includeAllPostPreview.js";
+import { makeCommonTheme } from "./themeLocaleconfig.js";
+import en from "./locales/en";
+import ru from "./locales/ru";
 
-
-export default {
+export default defineConfig({
   //head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   //head: [['link', {rel: 'stylesheet', href: '/tailwind.css'}]],
   head: [
-    ['script', {src: 'https://yastatic.net/share2/share.js'}],
+    ["script", { src: "https://yastatic.net/share2/share.js" }],
+    [
+      "script",
+      { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+    ],
   ],
-  outDir: '../docs',
-  cacheDir: '../.cache',
+  outDir: "../docs",
+  cacheDir: "../.cache",
   metaChunk: true,
   cleanUrls: true,
+  // lang: "en-US",
   sitemap: {
-    hostname: 'https://example.com'
+    hostname: "https://example.com",
   },
   locales: {
     root: {
-      lang: 'en-US',
+      lang: "en-US",
     },
     en: {
-      lang: 'en-US',
-      ...makeCommonTheme(en, 'en'),
+      lang: "en-US",
+      ...makeCommonTheme(en, "en"),
     },
     ru: {
-      lang: 'ru-RU',
-      ...makeCommonTheme(ru, 'ru'),
+      lang: "ru-RU",
+      ...makeCommonTheme(ru, "ru"),
     },
   },
   themeConfig: {
@@ -42,19 +47,18 @@ export default {
     showArticlePreview: true,
     // if previewText is not set then get it from description
     useDescriptionForArticlePreview: true,
-    tagsBaseUrl: '/tag',
-    allTagsUrl: '/tags',
-    archiveBaseUrl: '/archive',
-    recentBaseUrl: '/recent',
+    tagsBaseUrl: "/tag",
+    allTagsUrl: "/tags",
+    archiveBaseUrl: "/archive",
+    recentBaseUrl: "/recent",
   },
   transformPageData(pageData, ctx) {
-    transformTitle(pageData, ctx)
-    includeAllPostPreview(pageData, ctx)
+    transformTitle(pageData, ctx);
+    includeAllPostPreview(pageData, ctx);
   },
   markdown: {
     config: (md) => {
-      md.use(removeH1Plugin)
-    }
+      md.use(removeH1Plugin);
+    },
   },
-}
-
+});
--- src/index.md
+++ src/index.md
@@ -2,12 +2,29 @@
 import { useData, inBrowser } from 'vitepress'
 import { watchEffect } from 'vue'
 
-const { lang } = useData()
+const { site } = useData()
+const langindexes = Object.keys(site.value.locales)
+  .filter((item) => item !== 'root')
 
 watchEffect(() => {
-  if (inBrowser) {
-    window.location.replace('/' + navigator.language + '/');
+  if (inBrowser && window.location.pathname === '/') {
+    const langToRedirect = (langindexes.includes(navigator.language))
+      ? navigator.language
+      : 'en'
+
+    window.location.replace('/' + langToRedirect + '/');
   }
 })
 </script>
 
+<!-- <script> -->
+<!--   if (window.netlifyIdentity) { -->
+<!--     window.netlifyIdentity.on('init', user => { -->
+<!--       if (!user) { -->
+<!--         window.netlifyIdentity.on('login', () => { -->
+<!--           document.location.href = '/admin/'; -->
+<!--         }); -->
+<!--       } -->
+<!--     }); -->
+<!--   } -->
+<!-- </script> -->
